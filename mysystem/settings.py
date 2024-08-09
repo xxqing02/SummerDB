b9378fed26c790d76309f115472b7340a47ee7ff
jawung <jawung@163.com>
2024-08-09 14:47:23 +0800

    update

--- mysystem/settings.py
+++ mysystem/settings.py
@@ -165,9 +165,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email sender
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True  # 是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性)
-EMAIL_USE_SSL = False  # 是否使用SSL加密，qq企业邮箱要求使用，163邮箱设置为True的时候会报ssl的错误
-EMAIL_HOST = 'smtp.163.com'  # 发送邮件的邮箱的SMTP服务器，这里用的是163邮箱
-EMAIL_PORT = 465  # 发件箱的SMTP服务器端口，默认是25
-EMAIL_HOST_USER = 'jawung@163.com'  # 发送邮件的邮箱地址
-EMAIL_HOST_PASSWORD = 'PWEFQLLWUVGCVFJJ'  # 发送邮件的邮箱密码(这里使用的是授权码)
+EMAIL_USE_TLS = False 
+EMAIL_USE_SSL = True  # 使用SSL加密
+EMAIL_HOST = 'smtp.163.com'  # 163的SMTP服务器
+EMAIL_PORT = 465  # 使用SSL的端口
+EMAIL_HOST_USER = 'jawung@163.com'  # 您的163邮箱用户名
+EMAIL_HOST_PASSWORD = 'PYSVAULKUCANYNDB'  # 发送邮件的邮箱密码(这里使用的是授权码)
+# PYSVAULKUCANYNDB
+# PWEFQLLWUVGCVFJJ
